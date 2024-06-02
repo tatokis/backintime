tatokis/backintime

commit ba1ff7878040078eeb7c11e88378c1489e632a6a

feat: SSH proxy (jump) host (#1688)
Co-authored-by: Christian Buhtz <c.buhtz@posteo.jp>

qt/settingsdialog.py
@@ -72,8 +72,79 @@ import sshtools
 import logger
 from exceptions import MountException, NoPubKeyLogin, KnownHost
 
-# That value is used to wrap tooltip strings (inserting newline characters).
-_TOOLTIP_WRAP_LENGTH = 72
+
+class SshProxyWidget(QWidget):
+    """Used in SSH snapshot profiles on the General tab.
+
+    Dev note by buhtz (2024-04): Just a quick n dirty solution until the
+    re-design and re-factoring of the whole dialog.
+    """
+    def __init__(self, parent, host, port, user):
+        super().__init__(parent)
+
+        if host == '':
+            port = ''
+            user = ''
+
+        vlayout = QVBoxLayout(self)
+        # zero margins
+        vlayout.setContentsMargins(0, 0, 0, 0)
+
+        checkbox = QCheckBox(_('SSH Proxy'), self)
+        vlayout.addWidget(checkbox)
+        checkbox.stateChanged.connect(self._slot_checkbox_changed)
+
+        hlayout = QHBoxLayout()
+        vlayout.addLayout(hlayout)
+
+        hlayout.addWidget(QLabel(_('Host:'), self))
+        self.host_edit = QLineEdit(host, self)
+        hlayout.addWidget(self.host_edit)
+
+        hlayout.addWidget(QLabel(_('Port:'), self))
+        self.port_edit = QLineEdit(port, self)
+        hlayout.addWidget(self.port_edit)
+
+        hlayout.addWidget(QLabel(_('User:'), self))
+        self.user_edit = QLineEdit(user, self)
+        hlayout.addWidget(self.user_edit)
+
+        if host == '':
+            self._disable()
+
+        qttools.set_wrapped_tooltip(
+            self,
+            'Connect to the target host via this proxy (also known as a jump '
+            'host). See "-J" in the "ssh" command documentation or '
+            '"ProxyJump" in "ssh_config" man page for details.')
+
+    def _slot_checkbox_changed(self, state):
+        if Qt.CheckState(state) == Qt.CheckState.Checked:
+            self._enable()
+        else:
+            self._disable()
+
+    def _set_default(self):
+        self.host_edit.setText('')
+        self.port_edit.setText('22')
+        self.user_edit.setText(getpass.getuser())
+
+    def _disable(self):
+        self._set_default()
+        self._enable(False)
+
+    def _enable(self, enable=True):
+        # QEdit and QLabel's
+        lay = self.layout().itemAt(1)
+        for idx in range(lay.count()):
+            lay.itemAt(idx).widget().setEnabled(enable)
+
+    def values(self):
+        return {
+            'host': self.host_edit.text(),
+            'port': self.port_edit.text(),
+            'user': self.user_edit.text(),
+        }
 
 
 class SettingsDialog(QDialog):
@@ -189,7 +260,7 @@ class SettingsDialog(QDialog):
         hlayout.addWidget(self.btnSnapshotsPath)
         self.btnSnapshotsPath.clicked.connect(self.btnSnapshotsPathClicked)
 
-        # SSH
+        # --- SSH ---
         groupBox = QGroupBox(self)
         self.modeSsh = groupBox
         groupBox.setTitle(_('SSH Settings'))
@@ -256,6 +327,7 @@ class SettingsDialog(QDialog):
         self.btnSshKeyGen.setMinimumSize(32, 28)
         hlayout3.addWidget(self.btnSshKeyGen)
         self.btnSshKeyGen.clicked.connect(self.btnSshKeyGenClicked)
+
         # Disable SSH key generation button if a key file is already set
         self.txtSshPrivateKeyFile.textChanged \
             .connect(lambda x: self.btnSshKeyGen.setEnabled(not x))
@@ -263,6 +335,14 @@ class SettingsDialog(QDialog):
         qttools.equalIndent(self.lblSshHost,
                             self.lblSshPath,
                             self.lblSshCipher)
+
+        self.wdgSshProxy = SshProxyWidget(
+            self,
+            self.config.sshProxyHost(),
+            self.config.sshProxyPort(),
+            self.config.sshProxyUser()
+        )
+        vlayout.addWidget(self.wdgSshProxy)
 
         # encfs
         self.modeLocalEncfs = self.modeLocal
@@ -1027,10 +1107,11 @@ class SettingsDialog(QDialog):
         # one file system option
         self.cbOneFileSystem = QCheckBox(
             _('Restrict to one file system'), self)
-        self.cbOneFileSystem.setToolTip(
-            'uses \'rsync --one-file-system\'\n'
-            'From \'man rsync\':\n'
-            + '\n'.join(textwrap.wrap(
+        qttools.set_wrapped_tooltip(
+            self.cbOneFileSystem,
+            [
+                'uses \'rsync --one-file-system\'',
+                'From \'man rsync\':',
                 'This tells rsync to avoid crossing a filesystem boundary '
                 'when recursing. This does not limit the user\'s ability '
                 'to specify items to copy from multiple filesystems, just '
@@ -1038,8 +1119,8 @@ class SettingsDialog(QDialog):
                 'that the user specified, and also the analogous recursion '
                 'on the receiving side during deletion. Also keep in mind '
                 'that rsync treats a "bind" mount to the same device as '
-                'being on the same filesystem.',
-                _TOOLTIP_WRAP_LENGTH))
+                'being on the same filesystem.'
+            ]
         )
         layout.addWidget(self.cbOneFileSystem)
 
@@ -1296,7 +1377,7 @@ class SettingsDialog(QDialog):
         self.editSnapshotsPath.setText(
             self.config.snapshotsPath(mode='local'))
 
-        # ssh
+        # SSH
         self.txtSshHost.setText(self.config.sshHost())
         self.txtSshPort.setText(str(self.config.sshPort()))
         self.txtSshUser.setText(self.config.sshUser())
@@ -1500,6 +1581,10 @@ class SettingsDialog(QDialog):
         self.config.setSshHost(self.txtSshHost.text())
         self.config.setSshPort(self.txtSshPort.text())
         self.config.setSshUser(self.txtSshUser.text())
+        sshproxy_vals = self.wdgSshProxy.values()
+        self.config.setSshProxyHost(sshproxy_vals['host'])
+        self.config.setSshProxyPort(sshproxy_vals['port'])
+        self.config.setSshProxyUser(sshproxy_vals['user'])
         self.config.setSshSnapshotsPath(self.txtSshPath.text())
         self.config.setSshCipher(
             self.comboSshCipher.itemData(self.comboSshCipher.currentIndex()))
@@ -1680,6 +1765,9 @@ class SettingsDialog(QDialog):
                     self.config.sshUser(),
                     self.config.sshHost(),
                     port=str(self.config.sshPort()),
+                    proxy_user=self.config.sshProxyUser(),
+                    proxy_host=self.config.sshProxyHost(),
+                    proxy_port=self.config.sshProxyPort(),
                     askPass=tools.which('backintime-askpass'),
                     cipher=self.config.sshCipher()
                 )

common/config.py
@@ -653,6 +653,30 @@ class Config(configfile.ConfigFileWithProfiles):
     def setSshPrivateKeyFile(self, value, profile_id = None):
         self.setProfileStrValue('snapshots.ssh.private_key_file', value, profile_id)
 
+    def sshProxyHost(self, profile_id=None):
+        #?Proxy host used to connect to remote host.;;IP or domain address
+        return self.profileStrValue('snapshots.ssh.proxy_host', '', profile_id)
+
+    def setSshProxyHost(self, value, profile_id=None):
+        self.setProfileStrValue('snapshots.ssh.proxy_host', value, profile_id)
+
+    def sshProxyPort(self, profile_id=None):
+        #?Proxy host port used to connect to remote host.;0-65535
+        return self.profileIntValue(
+            'snapshots.ssh.proxy_host_port', '22', profile_id)
+
+    def setSshProxyPort(self, value, profile_id = None):
+        self.setProfileIntValue(
+            'snapshots.ssh.proxy_host_port', value, profile_id)
+
+    def sshProxyUser(self, profile_id=None):
+        #?Remote SSH user;;local users name
+        return self.profileStrValue(
+            'snapshots.ssh.proxy_user', getpass.getuser(), profile_id)
+
+    def setSshProxyUser(self, value, profile_id=None):
+        self.setProfileStrValue('snapshots.ssh.proxy_user', value, profile_id)
+
     def sshMaxArgLength(self, profile_id = None):
         #?Maximum command length of commands run on remote host. This can be tested
         #?for all ssh profiles in the configuration
@@ -698,16 +722,16 @@ class Config(configfile.ConfigFileWithProfiles):
         return args
 
     def sshCommand(self,
-                   cmd = None,
-                   custom_args = None,
-                   port = True,
-                   cipher = True,
-                   user_host = True,
-                   ionice = True,
-                   nice = True,
-                   quote = False,
-                   prefix = True,
-                   profile_id = None):
+                   cmd=None,
+                   custom_args=None,
+                   port=True,
+                   cipher=True,
+                   user_host=True,
+                   ionice=True,
+                   nice=True,
+                   quote=False,
+                   prefix=True,
+                   profile_id=None):
         """
         Return SSH command with all arguments.
 
@@ -726,20 +750,35 @@ class Config(configfile.ConfigFileWithProfiles):
         Returns:
             list:               ssh command with chosen arguments
         """
+        # Refactor: Use of assert is discouraged in productive code.
+        # Raise Exceptions instead.
         assert cmd is None or isinstance(cmd, list), "cmd '{}' is not list instance".format(cmd)
         assert custom_args is None or isinstance(custom_args, list), "custom_args '{}' is not list instance".format(custom_args)
-        ssh  = ['ssh']
+
+        ssh = ['ssh']
         ssh += self.sshDefaultArgs(profile_id)
+
+        # Proxy (aka Jump host)
+        if self.sshProxyHost(profile_id):
+            ssh += ['-J', '{}@{}:{}'.format(
+                self.sshProxyUser(profile_id),
+                self.sshProxyHost(profile_id),
+                self.sshProxyPort(profile_id)
+            )]
+
         # remote port
         if port:
             ssh += ['-p', str(self.sshPort(profile_id))]
+
         # cipher used to transfer data
         c = self.sshCipher(profile_id)
         if cipher and c != 'default':
-            ssh += ['-o', 'Ciphers={}'.format(c)]
+            ssh += ['-o', f'Ciphers={c}']
+
         # custom arguments
         if custom_args:
             ssh += custom_args
+
         # user@host
         if user_host:
             ssh.append('{}@{}'.format(self.sshUser(profile_id),
@@ -747,25 +786,32 @@ class Config(configfile.ConfigFileWithProfiles):
         # quote the command running on remote host
         if quote and cmd:
             ssh.append("'")
+
         # run 'ionice' on remote host
         if ionice and self.ioniceOnRemote(profile_id) and cmd:
             ssh += ['ionice', '-c2', '-n7']
+
         # run 'nice' on remote host
         if nice and self.niceOnRemote(profile_id) and cmd:
             ssh += ['nice', '-n19']
+
         # run prefix on remote host
         if prefix and cmd and self.sshPrefixEnabled(profile_id):
-            ssh += self.sshPrefixCmd(profile_id, cmd_type = list)
+            ssh += self.sshPrefixCmd(profile_id, cmd_type=type(cmd))
+
         # add the command
         if cmd:
             ssh += cmd
+
         # close quote
         if quote and cmd:
             ssh.append("'")
 
+        logger.debug(f'SSH command: {ssh}', self)
+
         return ssh
 
-    #ENCFS
+    # EncFS
     def localEncfsPath(self, profile_id = None):
         #?Where to save snapshots in mode 'local_encfs'.;absolute path
         return self.profileStrValue('snapshots.local_encfs.path', '', profile_id)
@@ -1311,17 +1357,25 @@ class Config(configfile.ConfigFileWithProfiles):
         self.setProfileBoolValue('snapshots.ssh.prefix.enabled', enabled, profile_id)
         self.setProfileStrValue('snapshots.ssh.prefix.value', value, profile_id)
 
-    def sshPrefixCmd(self, profile_id = None, cmd_type = str):
+    def sshPrefixCmd(self, profile_id=None, cmd_type=str):
+        """Return the config value of sshPrefix if enabled.
+
+        Dev note by buhtz (2024-04): Good opportunity to refactor. To much
+        implicit behavior in it.
+        """
         if cmd_type == list:
             if self.sshPrefixEnabled(profile_id):
                 return shlex.split(self.sshPrefix(profile_id))
-            else:
-                return []
+
+            return []
+
         if cmd_type == str:
             if self.sshPrefixEnabled(profile_id):
                 return self.sshPrefix(profile_id).strip() + ' '
-            else:
-                return ''
+
+            return ''
+
+        raise TypeError(f'Unable to handle type {cmd_type}.')
 
     def continueOnErrors(self, profile_id = None):
         #?Continue on errors. This will keep incomplete snapshots rather than

common/snapshots.py
@@ -732,8 +732,9 @@ class Snapshots:
             self.config.PLUGIN_MANAGER.error(1)
 
         elif (not force
-                  and self.config.noSnapshotOnBattery()
-                  and tools.onBattery()):
+              and self.config.noSnapshotOnBattery()
+              and tools.onBattery()):
+
             self.setTakeSnapshotMessage(
                 0, _('Deferring backup while on battery'))
             logger.info('Deferring backup while on battery', self)
@@ -761,7 +762,7 @@ class Snapshots:
                 logger.warning(
                     'A backup is already running. The pid of the already '
                     f'running backup is in file {instance.pidFile}. Maybe '
-                    'delete it.', self )
+                    'delete it.', self)
 
                 # a backup is already running
                 self.config.PLUGIN_MANAGER.error(2)
@@ -773,7 +774,7 @@ class Snapshots:
                     f'{restore_instance.pidFile}. Maybe delete it.', self)
 
             else:
-                if (self.config.noSnapshotOnBattery ()
+                if (self.config.noSnapshotOnBattery()
                         and not tools.powerStatusAvailable()):
                     logger.warning('Backups disabled on battery but power '
                                    'status is not available', self)
@@ -793,7 +794,7 @@ class Snapshots:
 
                 # mount
                 try:
-                    hash_id = mount.Mount(cfg = self.config).mount()
+                    hash_id = mount.Mount(cfg=self.config).mount()
 
                 except MountException as ex:
                     logger.error(str(ex), self)
@@ -3030,8 +3031,7 @@ class RootSnapshot(GenericNonSnapshot):
 
 def iterSnapshots(cfg, includeNewSnapshot = False):
     """
-    Iterate over snapshots in current snapshot path. Use this in a 'for' loop
-    for faster processing than list object
+    A generator to iterate over snapshots in current snapshot path.
 
     Args:
         cfg (config.Config):        current config
@@ -3042,21 +3042,33 @@ def iterSnapshots(cfg, includeNewSnapshot = False):
         SID:                        snapshot IDs
     """
     path = cfg.snapshotsFullPath()
+
     if not os.path.exists(path):
         return None
+
     for item in os.listdir(path):
+
         if item == NewSnapshot.NEWSNAPSHOT:
             newSid = NewSnapshot(cfg)
+
             if newSid.exists() and includeNewSnapshot:
                 yield newSid
+
             continue
+
         try:
             sid = SID(item, cfg)
+
             if sid.exists():
                 yield sid
+
+        # REFACTOR!
+        # LastSnapshotSymlink is an exception instance and could be catched
+        # explicit. But not sure about its purpose.
         except Exception as e:
             if not isinstance(e, LastSnapshotSymlink):
-                logger.debug("'{}' is not a snapshot ID: {}".format(item, str(e)))
+                logger.debug(
+                    "'{}' is not a snapshot ID: {}".format(item, str(e)))
 
 
 def listSnapshots(cfg, includeNewSnapshot = False, reverse = True):

common/sshtools.py
@@ -23,6 +23,7 @@ import socket
 import re
 import atexit
 import signal
+from pathlib import Path
 from time import sleep
 import logger
 import tools
@@ -103,6 +104,9 @@ class SSH(MountControl):
         self.host = None
         self.port = None
         self.cipher = None
+        self.proxy_user = None
+        self.proxy_host = None
+        self.proxy_port = None
         self.nice = None
         self.ionice = None
         self.nocache = None
@@ -122,6 +126,14 @@ class SSH(MountControl):
         self.setattrKwargs(
             'private_key_file',
             self.config.sshPrivateKeyFile(self.profile_id), **kwargs)
+
+        self.setattrKwargs(
+            'proxy_user', self.config.sshProxyUser(self.profile_id), **kwargs)
+        self.setattrKwargs(
+            'proxy_host', self.config.sshProxyHost(self.profile_id), **kwargs)
+        self.setattrKwargs(
+            'proxy_port', self.config.sshProxyPort(self.profile_id), **kwargs)
+
         self.setattrKwargs(
             'nice',
             self.config.niceOnRemote(self.profile_id), store=False, **kwargs)
@@ -201,6 +213,16 @@ class SSH(MountControl):
                      % ' '.join(sshfs),
                      self)
 
+        # SSH Proxy (aka Jump host)
+        if self.proxy_host:
+            sshfs.extend([
+                '-o',
+                'ssh_command=ssh -J '
+                f'{self.proxy_user}@{self.proxy_host}:{self.proxy_port}'
+            ])
+
+        logger.debug(f'Execute SSHFS command {sshfs}.')
+
         proc = subprocess.Popen(sshfs,
                                 env=env,
                                 stdout=subprocess.DEVNULL,
@@ -212,9 +234,12 @@ class SSH(MountControl):
         if proc.returncode == 0:
             return
 
-        raise MountException("{}\n\n{}".format(
-            _("Can't mount {sshfs}").format(sshfs=" ".join(sshfs)),
-            err))
+        raise MountException(
+            "{}\n\n{}".format(
+                _("Can't mount {sshfs}").format(sshfs=" ".join(sshfs)),
+                err
+            )
+        )
 
     def preMountCheck(self, first_run=False):
         """
@@ -434,14 +459,17 @@ class SSH(MountControl):
 
         logger.debug('Check login', self)
 
+        # Custom SSH arguments
+        custom_ssh_args = [
+            '-o',
+            'PreferredAuthentications=publickey',
+            '-p', str(self.port),
+            self.user_host
+        ]
+
+        # Create SSH command
         ssh = self.config.sshCommand(cmd=['exit'],
-                                     custom_args=[
-                                          '-o',
-                                          'PreferredAuthentications=publickey',
-                                          '-p',
-                                          str(self.port),
-                                          self.user_host
-                                     ],
+                                     custom_args=custom_ssh_args,
                                      port=False,
                                      user_host=False,
                                      nice=False,
@@ -691,7 +719,10 @@ class SSH(MountControl):
         if not self.config.sshCheckPingHost(self.profile_id):
             return
 
-        logger.debug('Check ping host', self)
+        ping_host = self.proxy_host or self.host
+        ping_port = self.proxy_port or self.port
+
+        logger.debug(f'Check ping host "{ping_host}:{ping_port}"', self)
         versionString = 'SSH-2.0-backintime_{}\r\n'.format(
             version.__version__).encode()
 
@@ -701,28 +732,29 @@ class SSH(MountControl):
 
             try:
                 with socket.create_connection(
-                        (self.host, self.port), 2.0) as s:
+                        (ping_host, ping_port), 2.0) as s:
 
                     result = s.connect_ex(s.getpeername())
                     s.sendall(versionString)
 
-            except:
+            except:  # Refactor: too broad exception
                 result = -1
 
             if result == 0:
-                logger.debug('Host %s is available' % self.host, self)
+                logger.debug(
+                    f'Host "{ping_host}:{ping_port}" is available', self)
                 return
-
-            logger.debug('Could not ping host %s. Try again' % self.host, self)
 
             count += 1
             sleep(0.2)
 
         if result != 0:
-            logger.debug('Failed pinging host %s' % self.host, self)
-
-            raise MountException(
-                f'Ping {self.host} failed. Host is down or wrong address.')
+            proxy_msg = f' via proxy "{ping_host}:{ping_port}"' \
+                if self.proxy_host else ''
+            msg = f'Ping {self.host}{proxy_msg} failed. ' \
+                  'Host is down or wrong address.'
+            logger.debug(msg, self)
+            raise MountException(msg)
 
     def checkRemoteCommands(self, retry=False):
         """
@@ -1030,8 +1062,76 @@ def sshKeyGen(keyfile):
     return not proc.returncode
 
 
-def sshCopyId(pubkey, user, host, port='22',
-              askPass='backintime-askpass', cipher=None):
+def sshCopyIdCommand(
+    pubkey,
+    user,
+    host,
+    port='22',
+    proxy_user=None,
+    proxy_host=None,
+    proxy_port='22',
+    cipher=None
+):
+    """
+    Generate a ssh-copy-id command to copy the given public ssh-key to a
+    remote host.
+
+    Args:
+        pubkey (str):   path to the public key file
+        user (str):     remote user
+        host (str):     remote host
+        port (str):     ssh port on remote host
+        proxy_user (str):     proxy host user
+        proxy_host (str):     proxy host
+        proxy_port (str):     proxy host port
+        cipher (str):   cipher used for ssh
+
+    Returns:
+        list: The ssh-copy-id command as a list.
+
+    Raises:
+        FileNotFoundError: If public key file not exist.
+    """
+    if not Path(pubkey).exists():
+        msg = f'SSH public key "{pubkey}" does not exist.'
+        logger.error(msg)
+        raise FileNotFoundError(msg)
+
+    cmd = [
+        'ssh-copy-id',
+        # key file
+        '-i', pubkey,
+        # port
+        '-p', str(port),
+    ]
+
+    # cipher
+    if cipher and cipher != 'default':
+        cmd.extend(['-o', 'Ciphers={}'.format(cipher)])
+
+    # proxy
+    if proxy_host:
+        proxy_jump = f'{proxy_user}@{proxy_host}:{proxy_port}'
+        cmd.extend(['-o', f'ProxyJump={proxy_jump}'])
+
+    cmd.append(f'{user}@{host}')
+
+    logger.debug(f'ssh-copy-id command {cmd}')
+
+    return cmd
+
+
+def sshCopyId(
+    pubkey,
+    user,
+    host,
+    port='22',
+    proxy_user=None,
+    proxy_host=None,
+    proxy_port=None,
+    askPass='backintime-askpass',
+    cipher=None
+):
     """
     Copy SSH public key ``pubkey`` to remote ``host``.
 
@@ -1046,13 +1146,16 @@ def sshCopyId(pubkey, user, host, port='22',
     Returns:
         bool:           True if successful
     """
-
-    if not os.path.exists(pubkey):
-        logger.warning(
-            'SSH public key "{}" does not exist. Skip copy to remote host'
-            .format(pubkey))
-
-        return False
+    cmd = sshCopyIdCommand(
+        pubkey,
+        user,
+        host,
+        port,
+        proxy_user,
+        proxy_host,
+        proxy_port,
+        cipher
+    )
 
     env = os.environ.copy()
     env['SSH_ASKPASS'] = askPass
@@ -1062,16 +1165,6 @@ def sshCopyId(pubkey, user, host, port='22',
             pubkey=pubkey, host=host),
         _('Please enter password for "{user}"').format(user=user)
     )
-
-    cmd = ['ssh-copy-id', '-i', pubkey, '-p', port]
-
-    if cipher and cipher != 'default':
-        cmd.extend(['-o', 'Ciphers={}'.format(cipher)])
-
-    cmd.append('{}@{}'.format(user, host))
-
-    logger.debug('Call command "{}"'.format(' '.join(cmd)))
-
     proc = subprocess.Popen(cmd, env=env,
                             stdout=subprocess.DEVNULL,
                             stderr=subprocess.PIPE,
@@ -1085,6 +1178,7 @@ def sshCopyId(pubkey, user, host, port='22',
     if proc.returncode:
         logger.error('Failed to copy ssh-key "{}" to "{}@{}": [{}] {}'
                      .format(pubkey, user, host, proc.returncode, err))
+
     else:
         logger.info('Successfully copied ssh-key "{}" to "{}@{}"'
                     .format(pubkey, user, host))

common/test/test_sshtools.py
@@ -17,6 +17,7 @@
 
 import os
 import sys
+import random
 import subprocess
 import stat
 import shutil
@@ -31,9 +32,10 @@ import sshtools
 import tools
 from exceptions import MountException
 
-@unittest.skipIf(not generic.LOCAL_SSH,
-                 'Skip as this test requires a local ssh server, public and '
-                 'private keys installed')
+SKIP_MESSAGE_SSH = 'Skip as this test requires a local ssh server, public ' \
+                   'and private keys installed'
+
+@unittest.skipIf(not generic.LOCAL_SSH, SKIP_MESSAGE_SSH)
 class TestSSH(generic.SSHTestCase):
     def test_can_mount_ssh_rw(self):
         mnt = mount.Mount(cfg = self.cfg, tmp_mount = True)
@@ -237,7 +239,7 @@ class TestSshKey(generic.TestCaseCfg):
             self.assertFalse(sshtools.sshKeyGen(secKey))
 
     @unittest.skipIf(getpass.getuser() != 'germar', 'Password login does not work on Travis-ci.')
-    @unittest.skipIf(not generic.LOCAL_SSH, 'Skip as this test requires a local ssh server, public and private keys installed')
+    @unittest.skipIf(not generic.LOCAL_SSH, SKIP_MESSAGE_SSH)
     def test_sshCopyId(self):
         with TemporaryDirectory() as tmp:
             secKey = os.path.join(tmp, 'key')
@@ -342,7 +344,7 @@ class TestSshKey(generic.TestCaseCfg):
                 if os.path.exists(knownHostsSic):
                     shutil.copyfile(knownHostsSic, knownHosts)
 
-@unittest.skipIf(not generic.LOCAL_SSH, 'Skip as this test requires a local ssh server, public and private keys installed')
+@unittest.skipIf(not generic.LOCAL_SSH, SKIP_MESSAGE_SSH)
 class TestStartSshAgent(generic.SSHTestCase):
     # running this test requires that user has public / private key pair created and ssh server running
     SOCK = 'SSH_AUTH_SOCK'
@@ -404,3 +406,84 @@ class TestStartSshAgent(generic.SSHTestCase):
         mockWhich.return_value = ''
         with self.assertRaises(MountException):
             self.ssh.startSshAgent()
+
+class SSHCopyID(unittest.TestCase):
+    def test_complete_command(self):
+        """Complete generated command"""
+        command = sshtools.sshCopyIdCommand(
+            generic.PRIV_KEY_FILE,
+            'user',
+            'non_existing_host',
+        )
+        self.assertEqual(
+            command,
+            [
+                'ssh-copy-id',
+                '-i',
+                generic.PRIV_KEY_FILE,
+                '-p',
+                '22',
+                'user@non_existing_host'
+            ]
+        )
+
+    def test_default_port(self):
+        """Default port"""
+        sut = sshtools.sshCopyIdCommand(
+            generic.PRIV_KEY_FILE,
+            'user',
+            'non_existing_host',
+        )
+        self.assertEqual(len(sut), 6, sut)
+        # no port explicit specified
+        self.assertEqual(sut[4], '22')
+
+    def test_custom_port(self):
+        """Custom (random) port"""
+        custom_port = str(random.randint(23, 128))
+
+        sut = sshtools.sshCopyIdCommand(
+            generic.PRIV_KEY_FILE,
+            'user',
+            'non_existing_host',
+            port=custom_port
+        )
+        self.assertEqual(sut[3], '-p')
+        self.assertEqual(sut[4], custom_port)
+
+    def test_proxy_with_default_port(self):
+        """Used proxy and default port"""
+        proxy_user = 'non_existing_proxy_user'
+        proxy_host = 'non_existing_proxy_host'
+
+        sut = sshtools.sshCopyIdCommand(
+            generic.PRIV_KEY_FILE,
+            'user',
+            'non_existing_host',
+            proxy_user=proxy_user,
+            proxy_host=proxy_host
+        )
+
+        self.assertIn(
+            'ProxyJump=non_existing_proxy_user@non_existing_proxy_host:22',
+            sut)
+
+    def test_proxy_with_custom_port(self):
+        """Used proxy and custom port"""
+        proxy_user = 'non_existing_proxy_user'
+        proxy_host = 'non_existing_proxy_host'
+        proxy_port = str(random.randint(23, 128))
+
+        sut = sshtools.sshCopyIdCommand(
+            generic.PRIV_KEY_FILE,
+            'user',
+            'non_existing_host',
+            proxy_user=proxy_user,
+            proxy_host=proxy_host,
+            proxy_port=proxy_port
+        )
+
+        self.assertIn(
+            'ProxyJump=non_existing_proxy_user@non_existing_proxy_host'
+            f':{proxy_port}',
+            sut)

commit 4221a01cc779334c8d672e1d95197176ca081622

fix(gui): SSHProxyWidget handle port values of type integer
Fix crash with SSHProxyWidget when port was of type int.

Bug was introduced with #1688

qt/settingsdialog.py
@@ -85,6 +85,9 @@ class SshProxyWidget(QWidget):
             port = ''
             user = ''
 
+        elif isinstance(port, int):
+            port = str(port)
+
         vlayout = QVBoxLayout(self)
         # zero margins
         vlayout.setContentsMargins(0, 0, 0, 0)